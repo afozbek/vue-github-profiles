4f5f3c5052e3176c99c6e8064e923cb9ed30dab7
afozbek <furkanozbek1995@gmail.com>
2020-07-29 23:35:59 +0300

    github api ları aktarıldı

---
+++ src/api/github-api.js
@@ -0,0 +1,42 @@
+export const current_user_url = "https=//api.github.com/user";
+export const current_user_authorizations_html_url =
+  "https=//github.com/settings/connections/applications{/client_id}";
+export const authorizations_url = "https=//api.github.com/authorizations";
+export const code_search_url =
+  "https=//api.github.com/search/code?q={query}{&page;per_page;sort;order}";
+export const commit_search_url =
+  "https=//api.github.com/search/commits?q={query}{&page;per_page;sort;order}";
+export const emails_url = "https=//api.github.com/user/emails";
+export const emojis_url = "https=//api.github.com/emojis";
+export const events_url = "https=//api.github.com/events";
+export const feeds_url = "https=//api.github.com/feeds";
+export const followers_url = "https=//api.github.com/user/followers";
+export const following_url = "https=//api.github.com/user/following{/target}";
+export const gists_url = "https=//api.github.com/gists{/gist_id}";
+export const hub_url = "https=//api.github.com/hub";
+export const issue_search_url =
+  "https=//api.github.com/search/issues?q={query}{&page;per_page;sort;order}";
+export const issues_url = "https=//api.github.com/issues";
+export const keys_url = "https=//api.github.com/user/keys";
+export const label_search_url =
+  "https=//api.github.com/search/labels?q={query}&repository_id={repository_id}{&page;per_page}";
+export const notifications_url = "https=//api.github.com/notifications";
+export const organization_url = "https=//api.github.com/orgs/{org}";
+export const organization_repositories_url =
+  "https=//api.github.com/orgs/{org}/repos{?type;page;per_page;sort}";
+export const organization_teams_url = "https=//api.github.com/orgs/{org}/teams";
+export const public_gists_url = "https=//api.github.com/gists/public";
+export const rate_limit_url = "https=//api.github.com/rate_limit";
+export const repository_url = "https=//api.github.com/repos/{owner}/{repo}";
+export const repository_search_url =
+  "https=//api.github.com/search/repositories?q={query}{&page;per_page;sort;order}";
+export const current_user_repositories_url =
+  "https=//api.github.com/user/repos{?type;page;per_page;sort}";
+export const starred_url = "https=//api.github.com/user/starred{/owner}{/repo}";
+export const starred_gists_url = "https=//api.github.com/gists/starred";
+export const user_url = "https=//api.github.com/users/{user}";
+export const user_organizations_url = "https=//api.github.com/user/orgs";
+export const user_repositories_url =
+  "https=//api.github.com/users/{user}/repos{?type;page;per_page;sort}";
+export const user_search_url =
+  "https=//api.github.com/search/users?q={query}{&page;per_page;sort;order}";
