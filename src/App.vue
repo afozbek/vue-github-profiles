9b1368bf13293ce4812601ad03721681c55930bc
afozbek <furkanozbek1995@gmail.com>
2020-07-30 11:14:51 +0300

    router düzenlendi

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,12 @@
 <template>
   <div class="m-container">
-    <div class="m-container__search" @keydown.enter="getUserData">
-      <b-field
-        class="m-container__searchInput"
-        label="Username"
-        type="is-success"
-        message="This username is available"
-      >
-        <b-input v-model="username" maxlength="30"></b-input>
-      </b-field>
-
-      <b-button
-        @click="getUserData"
-        class="m-container__searchButton"
-        type="is-success"
-      >
-        Search
-      </b-button>
-    </div>
-
-    <user-card-skeleton v-if="loading"></user-card-skeleton>
-    <user-card
-      v-else-if="!loading && hasUserLoaded"
-      :userData="user"
-    ></user-card>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import axios from "./github-instance";
-
-import UserCard from "@/components/UserCard.vue";
-import UserCardSkeleton from "@/components/Skeleton.vue";
-
 export default {
   name: "App",
-  data() {
-    return {
-      username: "afozbek",
-      user: {},
-      loading: false,
-      hasUserLoaded: false,
-    };
-  },
-  components: {
-    UserCard,
-    UserCardSkeleton,
-  },
-  methods: {
-    async getUserData() {
-      this.loading = true;
-
-      const result = await axios.get(`/users/${this.username}`);
-
-      this.user = result.data;
-      console.log({ ...result.data });
-
-      this.loading = false;
-      this.hasUserLoaded = true;
-    },
-  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import router from "./router";
 
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
@@ -9,5 +10,6 @@ Vue.use(Buefy);
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount("#app");
