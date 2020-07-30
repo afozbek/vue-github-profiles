99633127ff7783d276aa3267df25ab5cc0403b9c
afozbek <furkanozbek1995@gmail.com>
2020-07-30 11:50:51 +0300

    Home güncellendi

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,28 @@
 <template>
-  <search></search>
+  <div>
+    <search @user-search="userList = $event"></search>
+
+    <user-search-list :userList="userList"></user-search-list>
+  </div>
 </template>
 
 <script>
 import Search from "@/components/Home/Search.vue";
+import UserSearchList from "@/components/Home/UserList.vue";
 
 export default {
   name: "Home",
+  data() {
+    return {
+      userList: [],
+    };
+  },
   components: {
     Search,
+    UserSearchList,
+  },
+  methods: {
+    getUserList() {},
   },
 };
 </script>
