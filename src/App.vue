bb7964b35936a9b6888b62f6f2b7fff2d5638a8c
afozbek <furkanozbek1995@gmail.com>
2020-07-30 18:24:39 +0300

    header a toggle button eklendi

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,51 @@
 <template>
   <div class="m-container">
-    <header class="m-header">Github User Search App</header>
+    <header class="m-header">
+      Github User Search App
+      <div class="toggle-theme">
+        <input
+          class="toggle-theme__checkbox"
+          type="checkbox"
+          name="toggle-theme"
+          id="toggle-theme"
+        />
+
+        <label
+          class="toggle-theme__label"
+          for="toggle-theme"
+          @click="changeApplicationTheme"
+        >
+          <moon-icon></moon-icon>
+          <sun-icon></sun-icon>
+          <div class="ball"></div>
+        </label>
+      </div>
+    </header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import SunIcon from "@/components/icons/SunIcon.vue";
+import MoonIcon from "@/components/icons/MoonIcon.vue";
+
 export default {
   name: "App",
+  data() {
+    return {
+      lightTheme: true,
+    };
+  },
+  components: {
+    MoonIcon,
+    SunIcon,
+  },
+  methods: {
+    changeApplicationTheme() {
+      console.log("Değişiyor");
+      this.lightTheme = !this.lightTheme;
+    },
+  },
 };
 </script>
 
@@ -49,5 +87,48 @@ export default {
   font-size: 20px;
   font-weight: 500;
   box-shadow: 2px 2px 17px 0px rgba(0, 0, 0, 0.53);
+}
+
+.toggle-theme {
+  position: absolute;
+  right: 15px;
+  top: -5px;
+
+  &__label {
+    width: 70px;
+    height: 33px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    border: 1px solid #24292e;
+    border-radius: 30px;
+    padding: 3px;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    position: relative;
+    background: #000;
+
+    & .ball {
+      width: 28px;
+      height: 28px;
+      background-color: #fff;
+      border-radius: 50%;
+      transition: transform 0.4s;
+      position: absolute;
+      z-index: 10;
+      justify-self: flex-start;
+      top: 1.7px;
+      left: 2px;
+    }
+  }
+
+  &__checkbox {
+    visibility: hidden;
+  }
+
+  &__checkbox:checked ~ .toggle-theme__label .ball {
+    transform: translateX(37px);
+  }
 }
 </style>
