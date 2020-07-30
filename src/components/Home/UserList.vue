96a9e72591facd8ee6a279dedc4a7680b79e0720
afozbek <furkanozbek1995@gmail.com>
2020-07-30 11:50:43 +0300

    user-list eklendi

---
+++ src/components/Home/UserList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <user-card
+      v-for="user in userList"
+      :userData="user"
+      :key="user.id"
+    ></user-card>
+  </div>
+</template>
+
+<script>
+import UserCard from "./UserCard";
+
+export default {
+  name: "UserSearchList",
+  props: ["userList"],
+  components: {
+    UserCard,
+  },
+};
+</script>
